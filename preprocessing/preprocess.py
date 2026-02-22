cddd148f3422e14edf16c9008eac388a55d54e59
Siddharth <siddharth.shukla@adypu.edu.in>
2026-02-22 17:33:27 +0530

    Completed model training pipeline

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -8,19 +8,20 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('punkt_tab')
+
+# nltk.download("punkt")
+# nltk.download("stopwords")
+# nltk.download("wordnet")
+# nltk.download("punkt_tab")
 
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
 def load_data(file_path):
-    df=pd.read_csv(file_path ,encoding="utf-8")
+    df = pd.read_csv(file_path ,encoding = "utf-8")
     df["content"] = df["title"].fillna("") + " " + df["text"].fillna("")
     before_empty = len(df)
-    df=df[df['content'].str.strip()!='']
+    df=df[df["content"].str.strip()!=""]
     after_empty = len(df)
     print(f"Removed {before_empty-after_empty} rows with empty content.")
     before = len(df)
@@ -62,13 +63,12 @@ def preprocess_pipeline(text):
 def preprocess_entire_dataframe(df):
 
     print("\nApplying NLP preprocessing...")
-    tqdm.pandas()
-    df['processed_content'] = df['content'].progress_apply(preprocess_pipeline)
+    df["processed_content"] = df["content"].apply(preprocess_pipeline)
     return df
 
 def tfidf_vectorizer(df):
     print("\nCreating TF-IDF features...")
     vectorizer = TfidfVectorizer(max_features=5000,ngram_range=(1,2),min_df=5)
-    matrix = vectorizer.fit_transform(df['processed_content'])
+    matrix = vectorizer.fit_transform(df["processed_content"])
     print("matrix shape:", matrix.shape)
     return matrix,vectorizer
